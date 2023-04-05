affff0059bb21df3ce651cc8ebc9ed7926098ff0
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-04-05 19:29:35 +0530

    Add files via upload

---
+++ mathematical fun.py
@@ -0,0 +1,8 @@
+# 1st thing import math
+import math
+a=int(input("enter angle in degree"))
+b=math.radians(a)
+print("sin:",math.sin(b))
+print("cos:",math.cos(b))
+print("tan:",math.tan(b))
+d1=math.radians(input())
